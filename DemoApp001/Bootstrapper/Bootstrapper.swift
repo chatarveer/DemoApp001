ec488f9cc72c86a07e8223297df40182f0e8f761
Chatar Veer Suthar <chatarveer@gmail.com>
2019-09-28 16:31:48 +0400

    initial commit

---
+++ DemoApp001/Bootstrapper/Bootstrapper.swift
@@ -0,0 +1,37 @@
+//
+//  Bootstrapper.swift
+//  DemoApp001
+//
+//  Created by Veer Suthar on 28/09/2019.
+//  Copyright © 2019 Veer Suthar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Bootstrapper {
+    
+    var window: UIWindow
+    static var instance: Bootstrapper?
+    
+    static func initialize(windowScene: UIWindowScene) {
+        instance = Bootstrapper(window: makeNewWindow(windowScene: windowScene))
+        instance!.bootstrap()
+    }
+    
+    private static func makeNewWindow(windowScene: UIWindowScene) -> UIWindow {
+        let window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window.windowScene = windowScene
+        return window
+    }
+    
+    mutating func bootstrap() {
+        let controller = HomeRouter.createModule()
+        let navigationController = UINavigationController(rootViewController: controller)
+        if #available(iOS 13.0, *) {
+            navigationController.modalPresentationStyle = .fullScreen
+        }
+        self.window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+}
